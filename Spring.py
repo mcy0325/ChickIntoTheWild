b9fded2eea267ed889c5d1b2f71e11d556425d79
mcy0325 <mcy325@naver.com>
2023-11-23 16:51:28 +0900

    11 23 2

---
+++ Spring.py
@@ -0,0 +1,12 @@
+# import time
+# from Character import Character
+# from ImageLoader import ImageLoader
+
+# class Spring:
+#     egg_image_list = [image_loader.get_image("egg2"), image_loader.get_image("egg3")]
+
+#     while True:
+#         if joystick.button_L.value == False:  # L 버튼이 눌렸을 때
+#             egg.switch_images(egg_image_list, mirror=True)
+#         elif joystick.button_R.value == False:  # R 버튼이 눌렸을 때
+#             egg.switch_images(egg_image_list, mirror=False)
